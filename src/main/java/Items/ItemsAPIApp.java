cc5b87638c55de194e389984a7f01287b938e39c
Sefi <Sefishalom@gmail.com>
2018-08-14 21:39:46 +0300

    Initial commit

---
+++ src/main/java/Items/ItemsAPIApp.java
@@ -0,0 +1,13 @@
+package Items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsAPIApp {
+
+    public static void main (String[] args){
+        SpringApplication.run(ItemsAPIApp.class, args);
+    }
+
+}
